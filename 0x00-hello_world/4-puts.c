c8ffa3e4fed4f196772ad11baa46efe5e9001775
raziel362362 <lbt_alh@hotmail.fr>
2023-07-13 19:17:59 +0100

    hello, Puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,13 @@
 #include <stdio.h>
 
-
+/**
+*main - it will print puts
+*
+*Return: Always 0 (Success)
+*/
 
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-return 0;
+return (0);
 }
