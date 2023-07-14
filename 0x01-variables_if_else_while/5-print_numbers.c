9f07bdf1a459c47f3f8b0b217b6c20ca92a411e6
raziel362362 <lbt_alh@hotmail.fr>
2023-07-14 22:52:07 +0100

    Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - count from 0 to 9
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char i = 0;
+
+	while (i < 10)
+	{
+	printf("%d", i);
+	i++;
+	}
+	putchar('\n');
+return (0);
+}
