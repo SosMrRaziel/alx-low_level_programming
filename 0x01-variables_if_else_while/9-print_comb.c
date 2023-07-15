82a80f1f2a8fed52a82796959515347f9536eb47
raziel362362 <lbt_alh@hotmail.fr>
2023-07-15 01:55:41 +0100

    Patience, persistence and perspiration make an unbeatable combination for success

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -5,15 +5,13 @@ int main(void)
 {
 
 int nm = 48;
-char c = 44;
-char s = 32;
 for (; nm < 58 ; nm++)
 	{
 	putchar(nm);
 		if (nm != 57)
 		{
-		putchar(c);
-		putchar(s);
+		putchar(',');
+		putchar(' ');
 		}
 	}
 putchar('\n');
