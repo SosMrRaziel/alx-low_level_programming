0a03119fa5d9b2e629a8951be9ab8d1b5c75612c
raziel362362 <lbt_alh@hotmail.fr>
2023-07-19 03:03:59 +0100

    pew

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,24 +1,49 @@
-#include "main.h"
+#include"main.h"
 
-void jack_bauer(void)
+/**
+*jack_bauer-printeveryminuteoftheday
+*
+*Decription:printeveryminuteoftheday
+*
+*Return:void
+*/
+
+voidjack_bauer(void)
 {
-    for (int hour = 0; hour < 24; hour++)
-    {
-        for (int minute = 0; minute < 60; minute++)
-        {
-           _putchar((hour / 10) + '0');
-           _putchar((hour % 10) + '0');
-           _putchar(':');
-           _putchar((minute / 10) + '0');
-           _putchar((minute % 10) + '0');
-           _putchar('\n');
-        }
-    }
-}
+int hour_t, hour_o, minute_t, minute_o, hour_max;
 
-int main(void)
-{
-    jack_bauer();
-    return 0;
+	hour_max = 58;
+	hour_t = '0';
+	while (hour_t < '3')
+	{
+	if (hour_t == '2')
+	{
+	hour_max = '4';
+	}
+	hour_o = '0';
+		while (hour_o < hour_max)
+		{
+		minute_t = '0';
+		while (minute_t < '6')
+		{
+		minute_o = '0';
+		while (minute_o < 58)
+		{
+		_putchar(hour_t);
+		_putchar(hour_o);
+		_putchar(':');
+		_putchar(minute_t);
+		_putchar(minute_o);
+		_putchar('\n');
+		minute_o++;
+		}
+	minute_o = '0';
+	minute_t++;
+	}
+	minute_t = '0';
+	hour_o++;
+		}
+	hour_o = '0';
+	hour_t++;
+	}
 }
-
