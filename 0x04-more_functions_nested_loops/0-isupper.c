fdc23df56e777b0a7ce9e7c29525e39bb82decc7
raziel362362 <lbt_alh@hotmail.fr>
2023-07-20 16:58:20 +0100

    idk

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -6,7 +6,7 @@ int _isupper(int c)
 char ch_isupper;
 	for (ch_isupper = 'A'; ch_isupper >= 'Z'; ch_isupper++)
 	{
-		if (c = ch_isupper)
+		if (c == ch_isupper)
 		return (1);
 		else
 		return (0);
