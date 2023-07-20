40333b8d669407896f4b04fa48bc7f8800aa5191
raziel362362 <lbt_alh@hotmail.fr>
2023-07-20 17:41:50 +0100

    is upper

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -6,18 +6,10 @@
  */
 int _isupper(int c)
 {
-
-char ch_isupper;
-
-	for (ch_isupper = 'A'; ch_isupper <= 'Z'; ch_isupper++)
-	{
-		if (c == ch_isupper)
-	{
+	if (c >= 65 && c <= 90)
 		return (1);
-	}
-	else 
-	return (0);
-	}
+	else
+		return (0);
 
 }
 
