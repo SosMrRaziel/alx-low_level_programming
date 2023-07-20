bb8eee540755696aab7d53742560e2b72af6286f
raziel362362 <lbt_alh@hotmail.fr>
2023-07-20 22:46:38 +0100

    idk

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -7,6 +7,8 @@ void print_diagonal(int n)
 {
 	int i, j;
 
+	if (n > 0)
+	{
 	for (i = 0; i < n; i++)
 	{
 		for (j = 0; j < i; j++)
@@ -16,4 +18,7 @@ void print_diagonal(int n)
 	_putchar(92);
 	_putchar('\n');
 	}
+	}
+	else
+		_putchar('\n');
 }
