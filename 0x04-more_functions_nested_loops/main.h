a38c46b206dd15229ec6f940b9a8aa644535b638
raziel362362 <lbt_alh@hotmail.fr>
2023-07-20 18:16:30 +0100

    The numbers speak for themselves

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,9 +1,9 @@
-#ifndef HEADER_FILE
-#define HEADER_FILE
-
+#ifndef MAIN_H
+#define MAIN_H
+int _putchar(char c);
 int _isupper(int c);
 int _isdigit(int c);
 int mul(int a, int b);
 void print_numbers(void);
 
-endif
+#endif
