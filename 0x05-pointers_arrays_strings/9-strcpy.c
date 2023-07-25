3aafa4fcf7ac4c11756e999e04054a8a193adaf0
raziel362362 <lbt_alh@hotmail.fr>
2023-07-25 02:39:31 +0100

    strcpy

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -3,12 +3,14 @@
 
 char *_strcpy(char *dest, char *src)
 {
-    char *ptr = dest;
-    while (*src != '\0') {
-        *dest = *src;
-        dest++;
-        src++;
-    }
-    *dest = '\0';
-    return ptr;
+	char *ptr = dest;
+
+	while (*src != '\0')
+	{
+		*dest = *src;
+		dest++;
+		src++;
+	}
+	*dest = '\0';
+	return (ptr);
 }
