3bd4778e49ce65839222187138b23b524d67d56d
raziel362362 <lbt_alh@hotmail.fr>
2023-07-27 23:14:37 +0100

    idk

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,3 +1,6 @@
+#ifndef _main_H_
+#define _main_H_
+
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
 char *_strncpy(char *dest, char *src, int n);
@@ -6,5 +9,6 @@ void reverse_array(int *a, int n);
 char *string_toupper(char *);
 char *cap_string(char *);
 char *leet(char *);
-_putchar(char c);
+int _putchar(char);
 
+#endif
