cb13bcc0b8878d8f39f1b605157e25829b3a36e6
raziel362362 <lbt_alh@hotmail.fr>
2023-07-27 22:58:40 +0100

    strcat

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+
+char *_strcat(char *dest, char *src)
+{
+	char *ptr = dest;
+	while (*ptr != '\0')
+	{
+	ptr++;
+	}
+	while (*src != '\0')
+	{
+	*ptr++ = *src++;
+	}
+	*ptr = '\0';
+	return dest;
+}
+
