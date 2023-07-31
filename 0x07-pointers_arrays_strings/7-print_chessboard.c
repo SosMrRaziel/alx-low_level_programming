08473fbe8a64b41c1bf777197379a6440c5be7dc
raziel362362 <lbt_alh@hotmail.fr>
2023-08-01 00:19:27 +0100

    test

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -1,14 +1,15 @@
 #include "main.h"
 
 
-void print_chessboard(char (*a)[8]);
+void print_chessboard(char (*a)[8])
 {
 	int i, j;
+
 	for (i = 0; i < 8; i++)
 	{
 		for (j = 0; j < 8; j++)
 		{
-		_putchar(a[i][j];
+		_putchar(a[i][j]);
 		}
 	}
 }
