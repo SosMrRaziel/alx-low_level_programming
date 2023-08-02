269bf2107c6980dfaed4d96b406e434ad9aadf8d
raziel362362 <lbt_alh@hotmail.fr>
2023-08-02 21:20:27 +0100

    Your subconscious is looking for the dreamer

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,20 +1,10 @@
 #include "main.h"
 
-
-int _sqrt_recursive(int n)
+int _sqrt_recursion(int n)
 {
     if (n < 0)
         return (-1);
     if (n == 0 || n == 1)
         return (n);
-    return (_sqrt_recursive_helper(n, n / 2));
-}
-
-int _sqrt_recursive_helper(int n, int i)
-{
-    if (i * i == n)
-        return (i);
-    if (i <= 0)
-        return (-1);
-    return (_sqrt_recursive_helper(n, i - 1));
+    return (_sqrt_recursion(n / 2) * 2 == n ? _sqrt_recursion(n / 2) : -1);
 }
