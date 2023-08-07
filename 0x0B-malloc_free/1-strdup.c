6b4fb094b7f062c3e55a86f6575b56a4392d3e28
raziel362362 <lbt_alh@hotmail.fr>
2023-08-08 00:25:40 +0100

    test

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -9,7 +9,7 @@ char *_strdup(char *str)
 	if (str == 0)
 	return (NULL);
 
-	for ( i = 0; strdp[i] != '\0'; i++);
+	for ( i = 0; str[i] != '\0'; i++);
 	strdp = (char *) malloc(sizeof(char) * (i + 1));
 	if (strdp == 0)
 	return(NULL);
