969978ed789d02e0347a95db6029335a21599952
raziel362362 <lbt_alh@hotmail.fr>
2023-08-07 23:52:01 +0100

    test

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+
+char *_strdup(char *str)
+{
+	char *strdp
+	unsigned int i;
+
+	if (str == 0)
+	return (NULL);
+
+	for ( i = 0; strdp[i]; != '\0'; i++);
+	strdp = (char *) malloc(sizeof(char) * i);
+	if (strdp == 0)
+	return(NULL);
+
+
+
+
+}
