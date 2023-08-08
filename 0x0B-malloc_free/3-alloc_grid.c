e03efa902c5f6022a37b6b7b32fe33401009cc59
raziel362362 <lbt_alh@hotmail.fr>
2023-08-08 20:10:58 +0100

    If you even dream of beating me you'd better wake up and apologize

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+
+
+int **alloc_grid(int width, int height)
+{
+	int **dd;
+	int i, j;
+
+	if (width < 1 || height < 1)
+		return (NULL);
+
+	dd = (int **) malloc(height * sizeof(int));
+	if (dd == NULL)
+	{
+		free(dd);
+		return (NULL);
+	}
+	for (i = 0; i < height; i++)
+	{
+		dd[i] = (int *) malloc(width * sizeof(int));
+		if (dd[i] == NULL)
+		{
+			for (i--; i >= 0;i --)
+				return(NULL);
+		}
+	}
+	for (i = 0; i < height; i++)
+		for (j = 0; j < width; j++)
+			dd[i][j] = 0;
+return (dd);
+}
