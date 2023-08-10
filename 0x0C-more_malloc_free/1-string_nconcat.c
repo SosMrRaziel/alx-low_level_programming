f9c32d1c8965317e1ece2fb1ee20228880fd743e
raziel362362 <lbt_alh@hotmail.fr>
2023-08-10 17:49:46 +0100

    string_nconcat

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -14,7 +14,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 
 	if (n >= l2)
 		n = l2;
-	s =(char *) malloc(sizeof(char) * (l1 + l2 + 1));
+	s =(char *) malloc(sizeof(char) * (l1 + n + 1));
 		if (s == NULL)
 			return (NULL);
 
