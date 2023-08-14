a59e1c0e09d3c7c95fbd34e245f1bcec853f7390
raziel362362 <lbt_alh@hotmail.fr>
2023-08-14 17:03:10 +0100

    Poppy

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,5 +1,13 @@
 #ifndef DOG_H
 #define DOG_H
+/**
+ * struct dog - a struct that defines a dog
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
+ *
+ * Description: This struct defines a dog and its attributes.
+*/
 
 typedef struct dog
 	{
