a1aa361c2641a5afb3122581ba5456a262b664d5
raziel362362 <lbt_alh@hotmail.fr>
2023-08-29 00:37:00 +0100

    test

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -5,12 +5,13 @@ int pop_listint(listint_t **head)
 	listint_t *t;
 	int n;
 
-	if (!head)
+	if (!(*head))
 		return (0);
+
+	
 	n = (*head)->n;
 	t = *head;
 	*head = (*head)->next;
 	free(t);
 return (n);
 }
-
