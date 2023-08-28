6a77ae6ff8254c097824e40f710a706e6b5198d7
raziel362362 <lbt_alh@hotmail.fr>
2023-08-28 21:49:22 +0100

    list

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -1,5 +1,9 @@
 #include "lists.h"
-
+/**
+ * listint_len - count number of elements
+ * @h: the way of the sword
+ * Return: number of soliders
+ */
 size_t listint_len(const listint_t *h)
 {
 	int i;
