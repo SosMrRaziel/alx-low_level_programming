d815e4435f03958e7a500473d33bda9ed114a43a
raziel362362 <lbt_alh@hotmail.fr>
2023-08-29 21:14:59 +0100

    Delete

---
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -0,0 +1,30 @@
+#include "lists.h"
+
+
+int delete_nodeint_at_index(listint_t **head, unsigned int index)
+{
+
+	listint_t *now, *pre;
+	unsigned int i;
+
+	if (!(*head))
+		return (-1);
+
+	now = *head;
+	pre = NULL;
+
+	for (i = 0; now && i < index; i++)
+	{
+		pre = now;
+		now = now->next;
+	}
+	if (!now)
+		return (-1);
+	if (!pre)
+		*head = now->next;
+	else
+		pre->next = now->next;
+	free(now);
+		return (1);
+}
+		
