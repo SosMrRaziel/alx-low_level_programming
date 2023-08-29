7a4dd36e4576fca80c2179a9fc7d9aabb4a13fc6
raziel362362 <lbt_alh@hotmail.fr>
2023-08-29 18:52:14 +0100

    sum

---
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+/**
+*sum_listint - of all the data (n) of a listint_t linked list
+*@head: head of the linked list
+*Return: i
+*/
+
+int sum_listint(listint_t *head)
+{
+	int i;
+	listint_t *now = head;
+
+	for (i = 0; now != NULL; i++)
+	{
+		i += now->n;
+		now = now->next;
+	}
+	return (i);
+}
