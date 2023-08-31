f8b5eccc5f631449963d3d646227f56013438d49
raziel362362 <lbt_alh@hotmail.fr>
2023-08-31 23:02:51 +0100

    test

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int num = 0;
+	int i;
+
+	if (b == NULL)
+		return (0);
+
+	for (i = 0; b[i] != '\0'; i++)
+	{
+		if (b[i] != '0' && b[i] != '1')
+			return (0);
+	num = num << 1;
+	if (b[i] == '1')
+		num += 1;
+	}
+	return (num);
+}
