f7bb9eeb2cb8a2c081c1367a99d40f3a29eac805
raziel362362 <lbt_alh@hotmail.fr>
2023-09-05 01:16:29 +0100

    Under

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,5 +1,10 @@
 #include "main.h"
-
+/**
+ * create_file - creats a file without perm
+ * @filename: the filename
+ * @text_content: u who tell me
+ * Return: 1 if succeed -1 if not
+ */
 
 int create_file(const char *filename, char *text_content)
 {
