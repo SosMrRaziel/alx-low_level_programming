6f1d18d50ca1d6a66bddd6eeef0fc32662555e51
SosMrRaziel <lbt_alh@hotmail.fr>
2023-10-12 22:28:10 +0100

    task 8

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -1,4 +1,5 @@
 #include "lists.h"
+
 /**
  * delete_dnodeint_at_index - deletes the node at index of a dlistint_t list
  * @head: pointer to the head of the list
@@ -7,32 +8,34 @@
  */
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
-	dlistint_t *tmp;
-	unsigned int i;
+	dlistint_t *tmp; /*declare a temporary pointer to traverse the list*/
+	unsigned int i; /*declare a counter variable*/
 
-	if (head == NULL || *head == NULL)
-		return (-1);
+	if (head == NULL || *head == NULL) /* check if the list is empty or invalid*/
+		return (-1); /*return -1 to indicate failure*/
 
-	tmp = *head;
+	tmp = *head; /*assign the head pointer to the temporary pointer*/
 
-	if (index == 0)
+	if (index == 0) /*check if the node to delete is the first node*/
 	{
-		*head = tmp->next;
-		if (*head != NULL)
-			(*head)->prev = NULL;
-		free(tmp);
-		return (1);
+		*head = tmp->next; /*update the head pointer to point to the next node*/
+		if (*head != NULL) /*check if the next node is not NULL*/
+			(*head)->prev = NULL; /*set the prev pointer of the next node to NULL*/
+		free(tmp); /*free the memory of the first node*/
+		return (1); /*return 1 to indicate success*/
 	}
 
+/*loop until the index or the end of the list is reached*/
 	for (i = 0; i < index && tmp != NULL; i++)
-		tmp = tmp->next;
+		tmp = tmp->next; /*move the temporary pointer to the next node*/
 
-	if (tmp == NULL)
-		return (-1);
-
+	if (tmp == NULL) /*check if the index is out of range*/
+		return (-1); /*return -1 to indicate failure*/
+	/**update the next pointer of the previous node to point to the next node*/
 	tmp->prev->next = tmp->next;
-	if (tmp->next != NULL)
+	if (tmp->next != NULL) /*check if the next node is not NULL*/
+/*update the prev pointer of the next node to point to the previous node*/
 		tmp->next->prev = tmp->prev;
-	free(tmp);
-	return (1);
+	free(tmp); /*free the memory of the node to delete*/
+	return (1); /*return 1 to indicate success*/
 }
