671f0e976fdcb6f9ff09805d4596110481448614
SosMrRaziel <lbt_alh@hotmail.fr>
2023-10-12 21:02:00 +0100

    task 7

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -1,38 +1,42 @@
 #include "lists.h"
+
 /**
- * insert_dnodeint_at_index - Inserts a new node at a given position.
- * @h: A pointer to the head of the doubly linked list.
- * @idx: The index of the list where the new node should be added.
- * @n: The integer value of the new node.
- *
- * Return: The address of the new node, or NULL if it failed.
+ * insert_dnodeint_at_index - inserts a new node at a given position
+ * @h: pointer to the head of the list
+ * @idx: index of the list where the new node should be added
+ * @n: data of the new node
+ * Return: the address of the new node, or NULL if it failed
  */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	dlistint_t *new_node, *current;
+	dlistint_t *new, *current;
+	unsigned int i;
 
 	if (h == NULL)
 		return (NULL);
+
 	if (idx == 0)
 		return (add_dnodeint(h, n));
 
 	current = *h;
+	for (i = 0; i < idx - 1 && current != NULL; i++)
+		current = current->next;
 
 	if (current == NULL)
 		return (NULL);
 
-	if (idx == 1)
-	{
-		new_node = malloc(sizeof(dlistint_t));
-		if (new_node == NULL)
-			return (NULL);
-	new_node->n = n;
-	new_node->next = current;
-	new_node->next = current->next;
-	if (current->next != NULL)
-		current->next->prev = new_node;
-	current->next = new_node;
-	return (new_node);
-	}
-	return (insert_dnodeint_at_index(&(current->next), idx -1, n));
+	if (current->next == NULL)
+		return (add_dnodeint_end(h, n));
+
+	new = malloc(sizeof(dlistint_t));
+	if (new == NULL)
+		return (NULL);
+
+	new->n = n;
+	new->prev = current;
+	new->next = current->next;
+	current->next->prev = new;
+	current->next = new;
+
+	return (new);
 }
