b34678823ed7a46052d7a696509455e676a62d8b
SosMrRaziel <lbt_alh@hotmail.fr>
2023-11-03 23:16:51 +0100

    task 3

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -4,7 +4,8 @@
  * hash_table_set - adds an element to the hash table
  * @ht: the hash table to add or update the key/value to
  * @key: the key, cannot be an empty string
- * @value: the value associated with the key, must be duplicated, can be an empty string
+ * @value: the value associated with the key,
+ * must be duplicated, can be an empty string
  * Return: 1 if it succeeded, 0 otherwise
  */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
